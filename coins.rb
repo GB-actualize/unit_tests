06392b7990aa6c64cc1b78020fcdb73fd782c0eb
GB-actualize <gbactualize@gmail.com>
2016-09-22 21:09:09 -0500

    unit test refactored

--- coins.rb
+++ coins.rb
@@ -3,51 +3,19 @@ require 'rspec'
 class Coin
 
   def output(number)
+    denominations = [25,10,5,1]
     coin_array = []
 
-    while number > 0
-
-        if number >= 25
-          number -= 25
-          coin_array << "quarter"
-
-        
-        elsif number >= 10
-          number -= 10
-          coin_array << "dime"
-
-       
-        elsif number >= 5
-          number -= 5
-          coin_array << "nickel"
-
-        else number >= 1
-          number -= 1
-          coin_array << "penny" 
-
-        end
-
+    denominations.each do |denomination|
+      while number >= denomination
+        coin_array << denomination
+        number -= denomination
+      end
     end
-    # return "dime" if number % 10 == 0 
-    # return "nickel" if number % 5 == 0
-    # return "penny" if number % 1 == 0
-
     coin_array
   end
-
 end
 
-#   def go(number)
-#     coin_array = []
-#     if number % 25 == 0
-#       number -= 25
-#       coin_array << "quarter"
-#       coin_array
-#     end
-#   end
-# end
-
-# Coin.new.go
 Coin.new.output(119)
 
 RSpec.describe Coin do 
@@ -56,29 +24,8 @@ let(:coin)  { Coin.new }
   describe '#output' do
 
     it 'should return coin_array if given 119' do
-      expect(coin.output(119)).to eq(["quarter","quarter","quarter","quarter","dime","nickel","penny","penny","penny","penny",])
+      expect(coin.output(119)).to eq([25,25,25,25,10,5,1,1,1,1])
     end
-    # it 'should return "dime" if given 10' do
-    #   expect(coin.output(85)).to eq(["dime","quarter","quarter","quarter"])
-    # end
-
-    # it 'should return "nickel" if given 5' do
-    #   expect(coin.output(90)).to eq(["nickel", "dime","quarter","quarter","quarter"])
-    # end
-
-    # it 'should return "penny" if given 1' do
-    #   expect(coin.output(2)).to eq("penny")
-    # end
-
-    # it 'should return "nickel" if given 5' do
-    #   expect(coin.output(5)).to eq("nickel")
-    # end
-
-
-    # it 'should return "quarter" if given 25' do
-    #   expect(coin.output(83)).to eq("quarter")
-    # end
-
   end
 end
 
